ff7852f110233dee9fad77b563ac1b45e261149c
vman <vardhaman.rd@gmail.com>
2016-07-18 23:23:46 +0100

    moved config to an external file. created sourcemaps

--- MyTypeScript/gulpfile.js
+++ MyTypeScript/gulpfile.js
@@ -9,6 +9,9 @@ var tslint = require('gulp-tslint');
 var browserify = require("browserify");
 var source = require('vinyl-source-stream');
 var tsify = require("tsify");
+var buffer = require('vinyl-buffer');
+var sourcemaps = require('gulp-sourcemaps');
+var config = require('./gulpconfig.json');
 
 gulp.task('lint-ts', function () {
     return gulp.src('./Scripts/ts/*.ts')
@@ -30,17 +33,32 @@ gulp.task('browserify', ['lint-ts'], function () {
 
 gulp.task('minify-js', ['browserify'], function () {
     return gulp.src("./Output/app.js")
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(uglify())
     .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest("./Output"))
 });
 
-gulp.task('upload-to-sp', ['minify-js'], function () {
-    return gulp.src("./Output/*.js")
+gulp.task('upload-js-to-sp', ['minify-js'], function () {
+    return gulp.src("./Output/*.*")
       .pipe(spsave({
-          username: "user@tenant.onmicrosoft.com",
-          password: "password",
-          siteUrl: "https://tenant.sharepoint.com/sites/pub/",
+          username: config.userName,
+          password: config.password,
+          siteUrl: config.siteUrl,
+          folder: "Style Library/My Folder",
+          checkin: true,
+          checkinType: 1
+      }));
+});
+
+gulp.task('upload-html-to-sp', function () {
+    return gulp.src("./index.html")
+      .pipe(spsave({
+          username: config.userName,
+          password: config.password,
+          siteUrl: config.siteUrl,
           folder: "Style Library/My Folder",
           checkin: true,
           checkinType: 1
@@ -48,5 +66,5 @@ gulp.task('upload-to-sp', ['minify-js'], function () {
 });
 
 gulp.task('watch-ts-upload-to-sp', function () {
-    return gulp.watch("./Scripts/**/*.ts", ['upload-to-sp']);
+    return gulp.watch("./Scripts/**/*.ts", ['upload-js-to-sp']);
 });
